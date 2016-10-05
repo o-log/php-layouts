f3596ab2093fc66381dd0361749c0fb299fcf3d1
ologinov <olognv@gmail.com>
2016-10-05 15:53:15 +0300

    1

---
+++ OLOG/Layouts/InterfaceMenu.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OLOG\Layouts;
+
+interface InterfaceMenu
+{
+    static public function menuArr();
+}
--- OLOG/Layouts/LayoutsConfig.php
+++ OLOG/Layouts/LayoutsConfig.php
@@ -5,6 +5,40 @@ namespace OLOG\Layouts;
 class LayoutsConfig
 {
     static protected $admin_layout_class_name = LayoutDefault::class;
+    static protected $admin_site_title = 'SITE TITLE NOT SET';
+    static protected $admin_menu_classes_arr = [];
+
+    /**
+     * @return array
+     */
+    public static function getAdminMenuClassesArr()
+    {
+        return self::$admin_menu_classes_arr;
+    }
+
+    /**
+     * @param array $admin_menu_classes_arr
+     */
+    public static function setAdminMenuClassesArr($admin_menu_classes_arr)
+    {
+        self::$admin_menu_classes_arr = $admin_menu_classes_arr;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getAdminSiteTitle()
+    {
+        return self::$admin_site_title;
+    }
+
+    /**
+     * @param string $admin_site_title
+     */
+    public static function setAdminSiteTitle($admin_site_title)
+    {
+        self::$admin_site_title = $admin_site_title;
+    }
 
     /**
      * @return mixed
