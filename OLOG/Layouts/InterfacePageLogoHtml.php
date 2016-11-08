2f68dc1093056d6c2e638de7fcc4ffe0ebfa088b
Ninja <esomina@gmail.com>
2016-11-08 11:07:38 +0300

    1

---
+++ OLOG/Layouts/InterfacePageLogoHtml.php
@@ -0,0 +1,11 @@
+<?php
+namespace OLOG\Layouts;
+
+interface InterfacePageLogoHtml
+{
+    /**
+     * Возвращает html строку, содержащую лого страницы
+     * @return string
+     */
+    public function pageLogoHtml();
+}
