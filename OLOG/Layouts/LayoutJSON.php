d18daf3b413ab0321f5a9be937759cc5229180e5
ologinov <olognv@gmail.com>
2016-12-16 16:36:03 +0300

    1

--- OLOG/Layouts/LayoutJSON.php
+++ OLOG/Layouts/LayoutJSON.php
@@ -4,8 +4,8 @@ namespace OLOG\Layouts;
 
 class LayoutJSON implements InterfaceLayout
 {
-    static public function render($content, $action_obj){
+    static public function render($content, $action_obj, $encode_json_options = 0){
         header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($content);
+        echo json_encode($content, $encode_json_options);
     }
 }
