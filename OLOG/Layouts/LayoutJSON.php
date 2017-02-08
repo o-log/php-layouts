45feb4f1c1eb21312a429dd6bfe0ebc3668ac9a8
Антон Пинегин <pinegin.anton@gmail.com>
2017-02-08 15:19:15 +0300

    Fix

--- OLOG/Layouts/LayoutJSON.php
+++ OLOG/Layouts/LayoutJSON.php
@@ -2,10 +2,19 @@
 
 namespace OLOG\Layouts;
 
+use OLOG\Router;
+
 class LayoutJSON implements InterfaceLayout
 {
-    static public function render($content, $action_obj, $encode_json_options = 0){
+    static public function render($content, $action_obj, $encode_json_options = 0)
+    {
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($content, $encode_json_options);
+    }
+
+    static public function renderResponse($msg_arr, $success)
+    {
+        $action_obj = Router::getCurrentActionObj();
+        self::render(['success' => $success, 'msg_arr' => $msg_arr], $action_obj);
     }
 }
